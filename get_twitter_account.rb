396523eb57f6ae66eff47ec21f921c4609ba852f
alice345 <alice.maru345@gmail.com>
2015-03-24 08:53:36 +0900

    Twitterアカウント取得スクリプトの作成

---
+++ get_twitter_account.rb
@@ -0,0 +1,13 @@
+#メイドさんの公式Twitterアカウントを取得するためのスクリプト
+require 'anemone'
+require 'nokogiri'
+
+urls = [
+  premium = 'http://www.cafe-athome.com/maids/free/'
+  7f_maid = 'http://www.cafe-athome.com/maids/honten7f/'
+  6f_maid = 'http://www.cafe-athome.com/maids/honte16f/'
+  4f_maid = 'http://www.cafe-athome.com/maids/honten4f/'
+  donki_maid = 'http://www.cafe-athome.com/maids/donki/'
+  new_maid = 'http://www.cafe-athome.com/maids/new/'
+]
+
