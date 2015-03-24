dc69090cf62c312d361ca9c88f435b50e0655e03
alice345 <alice.maru345@gmail.com>
2015-03-25 00:09:27 +0900

    取得したURLからメイドさんの名前とお給仕情報を取得する
    一部のメイドさんについて，正確に取得できていないので要修正

--- get_twitter_account.rb
+++ get_twitter_account.rb
@@ -21,7 +21,15 @@ Anemone.crawl(urls, :depth_limit  => 1, :skip_query_strings => true) do |anemone
   end
 
   #取得したページに対する処理
-  anemone.on_pages_like(/maids\/\d{1,}/)do |page|
-    puts page.url
+  anemone.on_pages_like(/maids\/\d{1,}/) do |page|
+    doc = Nokogiri::HTML.parse(page.body)
+    
+    name = doc.xpath("//div[@id='maid-name']").text
+    twitter_account = doc.xpath("//div[@id='maid-properties']/dl/dd")[2].text
+    if twitter_account.match(/^@/)
+      puts "#{name} #{twitter_account} id:#{page.url.to_s.match(/\d{2,}/)}"
+    else
+      puts "#{name} no twitter account id:#{page.url.to_s.match(/\d{2,}/)}"
+    end
   end
 end
