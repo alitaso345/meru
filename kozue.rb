457636080354eb9ddfb6272055847919ac891119
alice345 <alice.maru345@gmail.com>
2015-03-25 16:23:55 +0900

    kozueをクラス化(動作確認はしてません...)
    できればTwitter周り全般を扱うクラスにしたい

--- kozue.rb
+++ kozue.rb
@@ -1,34 +1,34 @@
 require "twitter"
 require 'dotenv'
-Dotenv.load
-CONS_KEY = ENV['YOUR_CONSUMER_KEY']
-CONS_SEC = ENV['YOUR_CONSUMER_SECRET']
-ACCS_KEY = ENV['YOUR_ACCESS_TOKEN']
-ACCS_SEC = ENV['YOUR_ACCESS_SECRET']
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = CONS_KEY
-  config.consumer_secret     = CONS_SEC
-  config.access_token        = ACCS_KEY
-  config.access_token_secret = ACCS_SEC
-end
+class Kozue
+  def initialize
+    Dotenv.load
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['YOUR_CONSUMER_KEY'] 
+      config.consumer_secret     = ENV['YOUR_CONSUMER_SECRET'] 
+      config.access_token        = ENV['YOUR_ACCESS_TOKEN'] 
+      config.access_token_secret = ENV['YOUR_ACCESS_SECRET'] 
+    end
+  end
 
-def collect_with_max_id(collection=[], max_id=nil, &block)
-  response = yield(max_id)
-  collection += response
-  response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-end
+  def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = yield(max_id)
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
 
-def client.get_all_tweets(user)
-  collect_with_max_id do |max_id|
-    options = {count: 200, include_rts: true}
-    options[:max_id] = max_id unless max_id.nil?
-    user_timeline(user, options)
+  def get_all_tweets(user)
+    collect_with_max_id do |max_id|
+      options = {count: 200, include_rts: true}
+      options[:max_id] = max_id unless max_id.nil?
+      user_timeline(user, options)
+    end
   end
 end
 
-client.get_all_tweets("kozupi10akay").each do |tweet|
+Kozue.new.get_all_tweets("kozupi10akay").each do |tweet|
    p tweet.text if tweet.text.index("お給仕")
 end
-   # twitter gemのsearchの使い方わからなかったから、
-   # rubyのindexメソッドを使いました・・・なんか綺麗じゃない
+# twitter gemのsearchの使い方わからなかったから、
+# rubyのindexメソッドを使いました・・・なんか綺麗じゃない
