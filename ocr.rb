90ccd5f090a5e76e3cef58f3cb88273707c1be97
gaoch <misako3210@hotmail.com>
2015-03-25 15:09:26 +0900

    指定したURLの画像を現在のディレクトリに保存し、そこに書かれた内容を文字として出力

--- ocr.rb
+++ ocr.rb
@@ -1,7 +1,18 @@
 require 'tesseract-ocr'
+require 'open-uri'
+
+@path = 'http://pbs.twimg.com/media/B_-8jUpUYAAiX-2.jpg'
+
+fileName = File.basename(@path)
+
+open(fileName, 'wb') do |output|     #※１
+  open(@path) do |data|
+    output.write(data.read)          #※２
+  end
+end
 
 engine = Tesseract::Engine.new{ |engine|
     engine.language = :jpn
 }
 
-puts engine.text_for('shift_kozue.jpg')
+puts engine.text_for(fileName)
