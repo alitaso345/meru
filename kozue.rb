89f80b7c14d0ebb223fd9140ba350b243cd0a94d
alice345 <alice.maru345@gmail.com>
2015-03-27 01:48:27 +0900

    tweetから画像を取得するためのメソッドを定義
    中身は未実装

--- kozue.rb
+++ kozue.rb
@@ -1,15 +1,45 @@
 require "twitter"
-require 'dotenv'
 
 class Kozue
   def initialize
-    Dotenv.load
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['YOUR_CONSUMER_KEY'] 
-      config.consumer_secret     = ENV['YOUR_CONSUMER_SECRET'] 
-      config.access_token        = ENV['YOUR_ACCESS_TOKEN'] 
-      config.access_token_secret = ENV['YOUR_ACCESS_SECRET'] 
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
+    @console = Logger.new(STDOUT)
+    @log = Logger.new("output.log")
+  end
+
+  def get_image_from_twitter(maides_list)
+    begin
+      @console.info("Start get image from twitter")
+
+      maides_list.each do |maid|
+        p  maid
+        get_all_tweets(maid[:twitter_account]).each do |tweet|
+          @log.info(print_tweet(maid, tweet)) 
+          print_tweet(maid, tweet)
+        end
+      end
+      @console.info("Finish crawl twitter")
+    rescue => e
+      p e
+    end
+  end
+
+  private
+  def get_all_tweets(user)
+    collect_with_max_id do |max_id|
+      options = {count: 200, include_rts: true}
+      options[:max_id] = max_id unless max_id.nil?
+      @client.user_timeline(user, options)
+    end
+  end
+
+  def print_tweet(maid, tweet)
+    p "#{maid[:name]}: #{tweet.text}"
   end
 
   def collect_with_max_id(collection=[], max_id=nil, &block)
@@ -17,18 +47,4 @@ class Kozue
     collection += response
     response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
   end
-
-  def get_all_tweets(user)
-    collect_with_max_id do |max_id|
-      options = {count: 200, include_rts: true}
-      options[:max_id] = max_id unless max_id.nil?
-      user_timeline(user, options)
-    end
-  end
 end
-
-Kozue.new.get_all_tweets("kozupi10akay").each do |tweet|
-   p tweet.text if tweet.text.index("お給仕")
-end
-# twitter gemのsearchの使い方わからなかったから、
-# rubyのindexメソッドを使いました・・・なんか綺麗じゃない
