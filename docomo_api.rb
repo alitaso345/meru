9cb601a031b21c0b29b7507baf98647a73b5a3c3
gaoch <misako3210@hotmail.com>
2015-04-03 21:18:40 +0900

    docomoAPIのクラスを作成
    これでいいんだろうか・・・

--- docomo_api.rb
+++ docomo_api.rb
@@ -3,38 +3,55 @@ require 'json'
 
 API_KEY = ENV['DOCOMO_API_KEY']
 
-###   画像認識要求   ###
-uri = URI('https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/document')
-uri.query = 'APIKEY=' + API_KEY
+class DocomoAPI
+  def initialize
+    @id = 0
+    @uri = URI('https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/document')
+  end
 
-response = RestClient.post(
-  uri.to_s,
-  {:image => File.open('shift_kozue.jpg'),
-   :lang => 'jpn'
-  },
-  :content_type => 'multipart/form-data'
-)
+  ###   画像認識要求   ###
+  def req_ocr(picture)
+    @uri.query = 'APIKEY=' + API_KEY
 
-###   画像認識結果取得   ###
-json = JSON.parser.new(response)
-hash = json.parse()
+    response = RestClient.post(
+      @uri.to_s,
+      {:image => File.open(picture),
+       :lang => 'jpn'
+    },
+    :content_type => 'multipart/form-data'
+    )
 
-id = hash['job']['@id']
-uri.path += "/" +  id
+    # 得られた結果からidのみ出力
+    json = JSON.parser.new(response)
+    hash = json.parse()
+    @id = hash['job']['@id']
+  end
 
-loop do
-  result = RestClient.get(uri.to_s)
-  json2 = JSON.parser.new(result)
-  $hash2 = json2.parse()
-  if $hash2['job']['@status']=="success"
-    break
-  elsif $hash2['job']['@status']!="process"
-    p "失敗もしくは削除済みです"
-    exit
+  ###   画像認識結果取得   ###
+  def get_ocr
+    req_ocr('shift_kozue.jpg')
+    @uri.path += "/" +  @id
+
+    loop do
+      result = RestClient.get(@uri.to_s)
+      json2 = JSON.parser.new(result)
+      $hash = json2.parse()
+      # 処理に成功した場合のみ処理を続行
+      if $hash['job']['@status']=="success"
+        break
+      elsif $hash['job']['@status']!="process"
+        p "失敗もしくは削除済みです"
+        exit
+      end
+    end
+
+    # 得られたjsonのうち@textのみ出力
+    text =  $hash['lines']['line']   
+    text.each do |text|
+      p text['@text'] rescue nil
+    end
   end
 end
 
-text =  $hash2['lines']['line']
-text.each do |text|
-  p text['@text'] rescue nil
-end
+ocr=DocomoAPI.new
+ocr.get_ocr
