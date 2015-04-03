2b5f783f0148b773cef3b6ac36757975b1836964
gaoch <misako3210@hotmail.com>
2015-04-03 18:42:32 +0900

    uriのgemを導入して扱いやすくした

--- docomo_api.rb
+++ docomo_api.rb
@@ -3,16 +3,18 @@ require 'json'
 
 API_KEY = ENV['DOCOMO_API_KEY']
 
-uri = 'https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene?APIKEY=' + API_KEY
-
+###   情景画像認識要求   ###
+uri = URI('https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene')
+uri.query = 'APIKEY=' + API_KEY
 response = RestClient.post(
-  uri,
+  uri.to_s,
   {:image => File.open('shift_kozue.jpg'),
    :lang => 'jpn'
   },
   :content_type => 'multipart/form-data'
 )
 
+###   情景画像認識結果取得   ###
 json = JSON.parser.new(response)
 hash = json.parse()
 
