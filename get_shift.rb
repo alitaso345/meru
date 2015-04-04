a78f53e9c6eed5581ab0b1f85b40bbc7484e29a6
gaoch <misako3210@hotmail.com>
2015-04-04 13:32:44 +0900

    画像の中のお給仕日時情報(グレースケール部分)のみ取得するgrayscaleメソッドの作成

--- get_shift.rb
+++ get_shift.rb
@@ -2,7 +2,9 @@
 require "twitter"
 require 'dotenv'
 require 'open-uri'
+require "rmagick"
 require_relative "docomo_api"
+include Magick
 
 Dotenv.load
 CONS_KEY = ENV['TWITTER_CONSUMER_KEY']
@@ -46,15 +48,35 @@ class GetShift
     end
   end
 
+  # RMagickで画像のうちグレースケール部分のみ取得
+  def grayscale(file)
+    img = ImageList.new(file)
+    white = Magick::Pixel.new(255*256,255*256,255*256) # この色で塗りつぶす
+    for y in 0...img.rows
+      for x in 0...img.columns
+        src = img.pixel_color(x, y) # 元画像のピクセルを取得
+        rg = (src.red - src.green).abs
+        gb = (src.green - src.blue).abs
+        br = (src.blue - src.red).abs
+        if src==white # 白の場合は何もしない
+        elsif !((rg<10*256)&&(gb<10*256)&&(br<10*256)) # グレースケール以外の場合、白で塗りつぶす
+          img.pixel_color(x,y,white)
+        end
+      end
+    end
+      img.write file
+  end
+
   #userの"お給仕予定"が含まれたツイートの画像を取得
   def get_shift_img(user)
     @@client.get_all_tweets(user).each do |tweet|
       if tweet.text.index("お給仕予定")
         tweet.media.each do |media|
-          path = media.media_url.to_s          # @path=画像URL
+          path = media.media_url.to_s        # @path=画像URL
           save_file(path)                    # その画像をjpgで保存
           filename = File.basename(path)     # fileName="XXXX.jpg"
-          ocr = DocomoAPI.new                  # そのままDocomoAPIでocr
+          grayscale(filename)                # グレースケール部分のみ取得
+          ocr = DocomoAPI.new                # そのままDocomoAPIでocr
           ocr.req_ocr(filename)
           ocr.get_ocr
         end
