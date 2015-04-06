d092ac318b48b408fcc8fec5f4a133a48a9f2ffa
gaoch <misako3210@hotmail.com>
2015-04-06 14:37:12 +0900

    メソッドをprivateに変更

--- picture.rb
+++ picture.rb
@@ -17,6 +17,24 @@ class Picture
     end
   end
 
+  #userの"お給仕予定"が含まれたツイートの画像を取得
+  def get_shift_img(user)
+    get_all_tweets(user).each do |tweet|
+      if tweet.text.index("お給仕予定")
+        tweet.media.each do |media|
+          path = media.media_url.to_s        # @path=画像URL
+          save_file(path)                    # その画像をjpgで保存
+          filename = File.basename(path)     # fileName="XXXX.jpg"
+          grayscale(filename)                # グレースケール部分のみ取得
+          ocr = DocomoAPI.new                # そのままDocomoAPIでocr
+          ocr.req_ocr(filename)
+          ocr.get_ocr
+        end
+      end
+    end
+  end
+
+  private
   def collect_with_max_id(collection=[], max_id=nil, &block)
     response = yield(max_id)
     collection += response
@@ -59,24 +77,7 @@ class Picture
     end
       img.write file
   end
-
-  #userの"お給仕予定"が含まれたツイートの画像を取得
-  def get_shift_img(user)
-    get_all_tweets(user).each do |tweet|
-      if tweet.text.index("お給仕予定")
-        tweet.media.each do |media|
-          path = media.media_url.to_s        # @path=画像URL
-          save_file(path)                    # その画像をjpgで保存
-          filename = File.basename(path)     # fileName="XXXX.jpg"
-          grayscale(filename)                # グレースケール部分のみ取得
-          ocr = DocomoAPI.new                # そのままDocomoAPIでocr
-          ocr.req_ocr(filename)
-          ocr.get_ocr
-        end
-      end
-    end
-  end
-
+ 
   def grayscale?(rg,gb,br)
     if ((rg<10*256)&&(gb<10*256)&&(br<10*256))
       return true
