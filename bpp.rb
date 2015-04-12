62db5eb2483e6c914a7dc32f6468ea718ce81501
alice345 <alice.maru345@gmail.com>
2015-04-12 23:58:59 +0900

    実際の利用するときのイメージを追加

--- bpp.rb
+++ bpp.rb
@@ -5,4 +5,15 @@ require_relative "docomo_api"
 #picture.get_shift_img("mmmlln")
 
 api = DocomoAPI.new
-api.req_face_detection(File.open("./image/other.jpg"))
+file = File.open("./image/servise-time.jpg")
+
+result = api.req_face_detection(file)
+if result[:status] == "ok"
+  p "認証に成功しました"
+  p result[:data]['detectionFaceNumber']
+elsif result[:status] == "no_face"
+  p "人物画像ではありません"
+else
+  p "認証に失敗しました"
+  p result[:status]
+end
