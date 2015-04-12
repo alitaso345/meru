e15756c569d2dd0cb1c916bb656903b8ebb31b6a
alice345 <alice.maru345@gmail.com>
2015-04-12 22:52:58 +0900

    顔認識APIからレスポンスを得る

--- bpp.rb
+++ bpp.rb
@@ -1,5 +1,8 @@
 require_relative "picture"
+require_relative "docomo_api"
 
-picture= Picture.new
-picture.get_shift_img("mmmlln")
+#picture = Picture.new
+#picture.get_shift_img("mmmlln")
 
+api = DocomoAPI.new
+api.req_face_detection(File.open("./image/other.jpg"))
